23b56cb8b563354e3b7867216b71d249b4aa9372
indee <cindeevar@hotmail.com>
2022-09-14 13:30:36 -0400

    adding handlers

--- main.go
+++ main.go
@@ -1,37 +1,19 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-
-	"github.com/cindeevar/gomicroservices/handlers"
 )
 
 func main() {
 	//implementation through handler
-	l := log.New(os.Stdout, "")
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 	hh := handlers.NewHello(l)
-	//concrete implementation
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Hello World")
 
-		d, err := ioutil.ReadAll(r.Body)
+	sm := http.NewServeMux()
 
-		if err != nil {
-			http.Error(w, "oops", http.StatusBadRequest)
-			return
-		}
+	sm.Handle("/", hh)
 
-		log.Printf("Data %s \n", d)
-
-		fmt.Fprintf(w, "Hello %s", d)
-	})
-
-	http.HandleFunc("/welcome", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("GoodBye World")
-	})
-	http.ListenAndServe(":9000", nil)
+	http.ListenAndServe(":9090", nil)
 }
