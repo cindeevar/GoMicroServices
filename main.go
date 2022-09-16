479ddc9f19002a723044a3d28edf5415b08e6c0e
indee <cindeevar@hotmail.com>
2022-09-15 21:25:39 -0400

    adding package

--- main.go
+++ main.go
@@ -5,12 +5,13 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/cindeevar/GoMicroServices/handlers"
+	"github.com/cindeevar/GoMicroServices/tree/main/handlers"
 )
 
 func main() {
 	//implementation through handler
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+
 	hh := handlers.NewHello(l)
 
 	sm := http.NewServeMux()
